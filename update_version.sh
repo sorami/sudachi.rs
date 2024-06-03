d75d43d0de9f960255d2670216b8abaa438a046c
mh-northlander <mh.northlander+github@gmail.com>
2024-06-03 17:22:02 +0900

    update version-update script

--- update_version.sh
+++ update_version.sh
@@ -2,12 +2,12 @@
 
 set -eu
 
-if [ $# -lt 2 ] && [ "$1" != "show" ] ; then
+if [ $# -lt 1 ] || ( [ $# -lt 2 ] && [ "$1" != "show" ] ) ; then
     echo "Provide 2 arguments [from] and [to] to update version, or 'show' to print current one."
     exit 1
 fi
 
-VERSION_CRR=$(cat ./sudachi/Cargo.toml | grep -m1 "version \?=" | sed -r 's/^.*"(.*)".*$/\1/')
+VERSION_CRR=$(cat ./Cargo.toml | grep -m1 "version \?=" | sed -r 's/^.*"(.*)".*$/\1/')
 
 if [ $1 = "show" ] ; then
     echo ${VERSION_CRR}
@@ -25,13 +25,9 @@ fi
 # update
 echo "Update version from ${VERSION_FROM} to ${VERSION_TO}"
 
-CARGO_FILES="$(find . -name Cargo.toml)"
-
-for FILE in $CARGO_FILES ; do
-    echo $FILE
-    # replace the first occurrence of `^version = "<version>"$`
-    sed -i -r "1,/^version = / s/^version = \"${VERSION_FROM}\"$/version = \"${VERSION_TO}\"/" $FILE
-done
+WORKSPACE_CARGO_FILE="./Cargo.toml"
+echo $WORKSPACE_CARGO_FILE
+sed -i -r "1,/^version = / s/^version = \"${VERSION_FROM}\"$/version = \"${VERSION_TO}\"/" $WORKSPACE_CARGO_FILE
 
 PY_SETUP_FILE="./python/setup.py"
 echo $PY_SETUP_FILE

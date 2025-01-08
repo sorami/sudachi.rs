02298c11d603ecc0101a65a7af949f8722d8276a
mh-northlander <mh.northlander+github@gmail.com>
2025-01-08 14:20:25 +0900

    include resource files to embed and resolve workspace info in Cargo.toml

--- python/build-sdist.sh
+++ python/build-sdist.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 set -ex
 
-## Create a symlink for sudachi.rs
+## Create a symlink for sudachi.rs and resource to embed
 ln -sf ../sudachi sudachi-lib
-## Modify cargo.toml to include this symlink
+ln -sf ../resources resources
+
+## Resolve workspace.package value in Cargo.toml
+pip install tomlkit
+
+mv Cargo.toml Cargo.sudachipy.toml
+python modify_cargotoml_for_sdist.py \
+    ../Cargo.toml Cargo.sudachipy.toml --out Cargo.toml
+
+mv sudachi-lib/Cargo.toml Cargo.sudachilib.toml
+python modify_cargotoml_for_sdist.py \
+    ../Cargo.toml Cargo.sudachilib.toml --out sudachi-lib/Cargo.toml
+
+## Modify to include the symlink
 sed -i 's/\.\.\/sudachi/\.\/sudachi-lib/' Cargo.toml
 
 
@@ -12,9 +25,10 @@ python -m build --sdist
 
 
 # clean up changes
-## Modify cargo.toml
-sed -i 's/\.\/sudachi-lib/\.\.\/sudachi/' Cargo.toml
+## Revert cargo.toml
+rm Cargo.toml sudachi-lib/Cargo.toml
+mv Cargo.sudachipy.toml Cargo.toml
+mv Cargo.sudachilib.toml sudachi-lib/Cargo.toml
 
 ## rm files
-rm LICENSE sudachi-lib
-rm resources/ -rf
+rm LICENSE sudachi-lib resources

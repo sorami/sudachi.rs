b77a70f18630de7ce90d032db1b3cca52108a596
mh-northlander <mh.northlander+github@gmail.com>
2025-01-08 09:39:57 +0900

    use var for cargo target-triple

--- python/build-wheels-manylinux-pgo.sh
+++ python/build-wheels-manylinux-pgo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-# This script is assumed to be used inside https://github.com/pypa/manylinux.
+# Build wheels with PGO, inside a manylinux container (https://github.com/pypa/manylinux)
 
 DIR=$(dirname "$(readlink -f "$0")")
 
@@ -10,28 +10,29 @@ export PATH="$HOME/.cargo/bin:$PATH"
 
 cd "$DIR/.."
 
+TARGET_TRIPLE=$(rustc -vV | awk '/^host/ {print $2}')
 PROFDATA=/tmp/sudachi-profdata
 
 # Compile Binary that will generate PGO data
 RUSTFLAGS="-C profile-generate=$PROFDATA -C opt-level=3" \
-  cargo build --release -p sudachi-cli --target=x86_64-unknown-linux-gnu
+  cargo build --release -p sudachi-cli --target=$TARGET_TRIPLE
 
 # Download Kyoto Leads corpus original texts
 curl -L https://github.com/ku-nlp/KWDLC/releases/download/release_1_0/leads.org.txt.gz | gzip -dc > leads.txt
 
 # Generate Profile
-target/x86_64-unknown-linux-gnu/release/sudachi -o /dev/null leads.txt
-target/x86_64-unknown-linux-gnu/release/sudachi --wakati --mode=A -o /dev/null leads.txt
-target/x86_64-unknown-linux-gnu/release/sudachi --all --mode=B -o /dev/null leads.txt
+target/$TARGET_TRIPLE/release/sudachi -o /dev/null leads.txt
+target/$TARGET_TRIPLE/release/sudachi --wakati --mode=A -o /dev/null leads.txt
+target/$TARGET_TRIPLE/release/sudachi --all --mode=B -o /dev/null leads.txt
 
 # Generate Merged PGO data
-"$HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/bin/llvm-profdata" \
+"$HOME/.rustup/toolchains/stable-$TARGET_TRIPLE/lib/rustlib/$TARGET_TRIPLE/bin/llvm-profdata" \
   merge -o /tmp/sudachi-profdata.merged "$PROFDATA"
 
 cd "$DIR"
 
 export RUSTFLAGS='-C profile-use=/tmp/sudachi-profdata.merged -C opt-level=3'
-export CARGO_BUILD_TARGET=x86_64-unknown-linux-gnu
+export CARGO_BUILD_TARGET=$TARGET_TRIPLE
 
 # see following link for the list of cpython bin
 # https://github.com/pypa/manylinux?tab=readme-ov-file#image-content
